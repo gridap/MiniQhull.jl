9bba0a81ac074200ffca1c0f1ca6929f7b9869a3
victorsndvg <victorsv@gmail.com>
2020-02-18 10:25:28 +0100

    first approach to qhull wrapper bindings

--- src/MiniQhull.jl
+++ src/MiniQhull.jl
@@ -1,5 +1,22 @@
 module MiniQhull
 
-greet() = print("Hello World!")
+using Libdl
+
+export delaunay_init_and_compute
+export delaunay_fill_cells
+export delaunay_free
+export delaunay
+
+include("load.jl")
+include("bindings.jl")
+
+function delaunay(dim::Int32,numpoints::Int32,points::Vector{Float64}) 
+    numcells = Ref{Int32}()
+    delaunay_init_and_compute(dim,numpoints,points,numcells)
+    cells = Vector{Int32}(undef,(dim+1)*numcells[])
+    delaunay_fill_cells(dim, numcells[], cells)
+    delaunay_free()
+    cells
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using MiniQhull
 using Test
 
 @testset "MiniQhull.jl" begin
-    # Write your own tests here.
+    dim = Int32(2)
+    numpoints=Int32(4)
+    points=Vector{Float64}([0,0,0,1,1,0,1,1])
+    @test delaunay(dim,numpoints,points) == [4,2,1,4,3,1]
 end
