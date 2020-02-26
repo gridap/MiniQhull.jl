5062610d7f36023f054f42d639f0eaa8d7e85a8f
victorsndvg <victorsv@gmail.com>
2020-02-26 10:08:39 +0100

    update tests to use both vector and array inputs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,6 @@
 using MiniQhull
 using Test
 
-@testset "MiniQhull.jl" begin
-    dim = Int32(2)
-    numpoints=Int32(4)
-    points=Vector{Float64}([0,0,0,1,1,0,1,1])
-    @test delaunay(dim,numpoints,points) == [4,2,1,4,3,1]
+@testset "MiniQhull" begin
+    @test delaunay(2,4,[0,0,0,1,1,0,1,1]) == delaunay([0 0 1 1; 0 1 0 1])
 end
