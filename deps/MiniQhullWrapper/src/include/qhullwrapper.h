67d37b94c219a2b123dfce2e06c6c04a5025f122
victorsndvg <victorsv@gmail.com>
2020-02-26 10:29:56 +0100

    fix MiniQhullWrapper headers

--- deps/MiniQhullWrapper/src/include/qhullwrapper.h
+++ deps/MiniQhullWrapper/src/include/qhullwrapper.h
@@ -2,7 +2,7 @@
 #include <libqhull_r/libqhull_r.h>
 
 qhT* new_qhull_handler();
-int delaunay_init_and_compute_r(qhT *qh, int dim, int numpoints, coordT *points, int* numcells);
-int delaunay_fill_cells_r(qhT *qh, int dim, int num_cells, int *cells);
-int delaunay_free_r(qhT *qh);
+int delaunay_init_and_compute(qhT *qh, int dim, int numpoints, coordT *points, int* numcells);
+int delaunay_fill_cells(qhT *qh, int dim, int num_cells, int *cells);
+int delaunay_free(qhT *qh);
 
