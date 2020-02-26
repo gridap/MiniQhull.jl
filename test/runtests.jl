63c73accf28324f2e35ea0fbbac2c2f4b364e465
victorsndvg <victorsv@gmail.com>
2020-02-26 13:20:36 +0100

    conditional tests if qhull wrapper is correctly loaded

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using MiniQhull
 using Test
 
-@testset "MiniQhull" begin
-    @test delaunay(2,4,[0,0,0,1,1,0,1,1]) == delaunay([0 0 1 1; 0 1 0 1])
+if MiniQhull.QHULL_WRAPPER_LOADED[]
+    @testset "MiniQhull" begin
+        @test delaunay(2,4,[0,0,0,1,1,0,1,1]) == delaunay([0 0 1 1; 0 1 0 1])
+    end
 end
